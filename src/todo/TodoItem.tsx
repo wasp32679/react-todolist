649541fd61fe998b44e27b73206e0e5679a534d2
wasp32679 <ryan.kenmoe@outlook.fr>
2026-01-20 09:36:52 +0100

    chore: changed todoitem html

--- src/todo/TodoItem.tsx
+++ src/todo/TodoItem.tsx
@@ -3,15 +3,15 @@ import trash from '../assets/trash-bin.png';
 export default function TodoItem() {
   return (
     <li>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente iure
-        expedita repellendus ducimus officiis consequatur, veniam facilis
-        exercitationem, quaerat ipsa explicabo voluptates quam unde architecto
-        velit commodi eos, accusantium tempora!
-      </p>
-      <button>
-        <img src={trash} alt="supprimer" className="item-img" />
-      </button>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente iure
+      expedita repellendus ducimus officiis consequatur, veniam facilis
+      exercitationem, quaerat ipsa explicabo voluptates quam unde architecto
+      velit commodi eos, accusantium tempora!
+      <div>
+        <button>
+          <img src={trash} alt="supprimer" className="item-img" />
+        </button>
+      </div>
     </li>
   );
 }
