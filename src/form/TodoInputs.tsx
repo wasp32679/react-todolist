3419659f5f50f658db32ef4e19d934ed0a4c235c
wasp32679 <ryan.kenmoe@outlook.fr>
2026-01-20 13:27:45 +0100

    feat: added css close #5

--- src/form/TodoInputs.tsx
+++ src/form/TodoInputs.tsx
@@ -1,9 +1,14 @@
+import './TodoForm.css';
+
 export default function TodoInputs() {
   return (
     <>
-      <input type="text" id="title" />
-      <input type="date" id="due-date" />
-      <input type="text" id="description" />
+      <label htmlFor="title">Title :</label>
+      <input type="text" id="title" className="shadow" />
+      <label htmlFor="description">Description :</label>
+      <textarea className="shadow"></textarea>
+      <label htmlFor="due-date">Due-Date :</label>
+      <input type="date" id="due-date" className="shadow" />
     </>
   );
 }
--- src/todo/Filter.tsx
+++ src/todo/Filter.tsx
@@ -1,7 +1,13 @@
+import './Filter.css';
+
 export default function Filter() {
   return (
     <>
-      <select name="filter" id="filter-select">
+      <select
+        name="filter"
+        id="filter-select"
+        className="border small-el shadow"
+      >
         <option>All</option>
       </select>
     </>
