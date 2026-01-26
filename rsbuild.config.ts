3cfbeb3b2c66cc01bbf24599898e8aa5da45ae75
wasp32679 <ryan.kenmoe@outlook.fr>
2026-01-26 08:51:23 +0100

    chore: changed pnpm, node versions in action

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,7 +4,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   output: {
-assetPrefix: "/react-todolist/"
+  assetPrefix: "/react-todolist/",
   },
   plugins: [pluginReact()],
 });
