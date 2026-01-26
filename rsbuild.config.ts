393fa08ae1b77e65bce60dfa3bd7d7bc1ee57fb1
wasp32679 <ryan.kenmoe@outlook.fr>
2026-01-26 08:41:56 +0100

    ci: configure GitHub Pages deployment close #6

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  output: {
+assetPrefix: "/react-todolist/"
+  },
   plugins: [pluginReact()],
 });
