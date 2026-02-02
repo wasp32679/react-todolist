9c9fe259385c91444f4f96634b76d18ca92699aa
wasp32679 <ryan.kenmoe@outlook.fr>
2026-02-02 15:41:04 +0100

    feat: can now sort and filter todos close #11

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,47 @@ const todoPromise = fetchTodosFromApi();
 export default function App() {
   const initialTodos = use(todoPromise);
   const [todos, setTodos] = useState<ReadTodo[]>(initialTodos);
+  const [sort, setSort] = useState('');
+  const [filter, setFilter] = useState('');
+
+  let sortedTodos = [...todos];
+
+  if (sort === 'name') {
+    sortedTodos.sort((a, b) => a.title.localeCompare(b.title));
+  }
+  if (sort === 'due-date') {
+    sortedTodos.sort((a, b) => {
+      if (!a.due_date && !b.due_date) return 0;
+      if (!a.due_date) return 1;
+      if (!b.due_date) return -1;
+      return a.due_date > b.due_date ? 1 : -1;
+    });
+  }
+
+  if (filter === 'undone') {
+    sortedTodos = sortedTodos.filter((t) => !t.done);
+  }
+  if (filter === 'done') {
+    sortedTodos = sortedTodos.filter((t) => t.done);
+  }
+
+  const handleSortChange = (newSortValue: string) => {
+    setSort(newSortValue);
+  };
+
+  const handleFilterChange = (newFilterValue: string) => {
+    setFilter(newFilterValue);
+  };
 
   return (
     <div className="app-section">
       <h1>TODO APP</h1>
       <div className="top-controls">
         <OpenAddTodoFormBtn setTodos={setTodos} />
-        <Filter />
+        <Filter filterValue={handleFilterChange} />
       </div>
-      <SortBy />
-      <Todolist todos={todos} setTodos={setTodos} />
+      <SortBy sortValue={handleSortChange} />
+      <Todolist todos={sortedTodos} setTodos={setTodos} />
       <div className="delete-all-wrapper">
         <DeleteAllTodosBtn />
       </div>
--- src/todo/Sort.tsx
+++ src/todo/Sort.tsx
@@ -1,10 +1,23 @@
+import type { SortProps } from '../types/todo';
 import './Sort.css';
 
-export default function SortBy() {
+export default function SortBy({ sortValue }: SortProps) {
+  const setSorting = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    sortValue(value);
+  };
+
   return (
     <>
-      <select name="sort" id="sort-select" className="border small-el shadow">
+      <select
+        onChange={setSorting}
+        name="sort"
+        id="sort-select"
+        className="border small-el shadow"
+      >
         <option>Sort</option>
+        <option value={'name'}>Name</option>
+        <option value={'due-date'}>Due date</option>
       </select>
     </>
   );
